98a380fbdba16a333c1cb03e5fed42868f1f1b33
Toumany Doumbouya <toumany-doumbouya@td.lan>
2024-05-03 12:14:55 +0200

    Modif dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,7 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-import java.io.ObjectInputFilter.Status;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.algorithm.utils.ElementNotFoundException;
 import org.insa.graphs.model.Arc;
@@ -11,7 +13,7 @@ import org.insa.graphs.model.Path;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
-    protected void initLabel( Label[] labelTab, ShortestPathData data){
+    protected void initLabel(Label[] labelTab, ShortestPathData data) {
         for (Node node : data.getGraph().getNodes()) {
             labelTab[node.getId()] = new Label(node);
         }
@@ -25,61 +27,66 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        
-        // TODO:
 
         Graph ourGraph = data.getGraph();
         final int nbNodes = ourGraph.size();
 
-        Label [] labels = new Label[nbNodes];
+        Label[] labels = new Label[nbNodes];
 
         initLabel(labels, data);
 
         BinaryHeap<Label> tasBinaire = new BinaryHeap<Label>();
 
-        labels[data.getOrigin().getId()].setCost(0);;
+        labels[data.getOrigin().getId()].setCost(0);
+        ;
         tasBinaire.insert(labels[data.getOrigin().getId()]);
 
         Label x;
 
-        while ( !(tasBinaire.isEmpty()) 
+        while (!(tasBinaire.isEmpty())
                 && !(labels[data.getDestination().getId()].getMarked())) {
-            
+
             x = tasBinaire.deleteMin();
             x.setMarked(true);
-            
+
             for (Arc arc : x.getSommetCourant().getSuccessors()) {
                 Node succDesti = arc.getDestination();
-                if ( !(labels[succDesti.getId()].getMarked()) ) {
-                    if (
-                        (labels[succDesti.getId()].getCost() > labels[x.getSommetCourant().getId()].getCost() + data.getCost(arc)  
-                        && data.isAllowed(arc))
-                    ) {
+                if (!(labels[succDesti.getId()].getMarked())) {
+                    if ((labels[succDesti.getId()].getCost() > labels[x.getSommetCourant().getId()].getCost()
+                            + data.getCost(arc)
+                            && data.isAllowed(arc))) {
                         try {
                             tasBinaire.remove(labels[succDesti.getId()]);
-                        } catch (ElementNotFoundException e) {}
+                        } catch (ElementNotFoundException e) {
+                        }
 
                         labels[succDesti.getId()].setPere(arc);
-                        labels[succDesti.getId()].setCost(labels[x.getSommetCourant().getId()].getCost()  + data.getCost(arc));
+                        labels[succDesti.getId()]
+                                .setCost(labels[x.getSommetCourant().getId()].getCost() + data.getCost(arc));
                         labels[succDesti.getId()] = new Label(succDesti);
                     }
                 }
             }
         }
 
-
         if ((labels[data.getDestination().getId()].getMarked())) {
-            Node[] listPereSucc;
+            ArrayList<Arc> listPereSucc = new ArrayList<Arc>();
 
-            for (int i = 0; i < labels.length ; i++){
-                listPereSucc[i] = labels[]
+            for (int i = 0; i < labels.length; i++) {
+                listPereSucc.add(i, labels[i].getPere());
             }
+            
+            Collections.reverse(listPereSucc);
 
             Path finalPath = new Path(ourGraph, listPereSucc);
-            solution = new ShortestPathSolution(data, Status.ALLOWED, finalPath);
-        } else{
+            System.out.println("OPTIMAL");
 
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, finalPath);
+        } else {
+            System.out.println("INFEASIBLE");
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
         }
+
         return solution;
     }
 
